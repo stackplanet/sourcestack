104d1902df93fd6713e269b45d58b33433e28a82
Martin Pllu <jmpllu@gmail.com>
2020-05-02 23:05:40 +0100

    Log message

--- infra/src/use-backend.ts
+++ infra/src/use-backend.ts
@@ -6,6 +6,7 @@ let jwkToPem = require('jwk-to-pem');
 
 (async () => {
     Config.ensureArgsSupplied();
+    console.log('Using backend ' + Config.appEnv())
     let stackOutputs = await getStackOutput(Config.appEnv());
     await writeBackendConfig('../backend/', stackOutputs);
     console.log('Local server configured to use backend ' + Config.appEnv() + '. Please restart the server.');
