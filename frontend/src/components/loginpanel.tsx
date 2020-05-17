3957c31bc0c5cc6202e6e767aad2f5ddb127f800
Martin Pllu <jmpllu@gmail.com>
2020-05-17 09:48:19 +0100

    Extract LoginPanel component

---
+++ frontend/src/components/loginpanel.tsx
@@ -0,0 +1,24 @@
+import { MithrilTsxComponent } from "mithril-tsx-component";
+import m, { Vnode } from "mithril";
+import { Page } from "./page";
+
+interface Attrs {
+    title: string;
+}
+
+export class LoginPanel extends MithrilTsxComponent<Attrs> {
+
+    view(vnode: Vnode<Attrs>) {
+        return <Page hideNavbar={true}>
+            <div class="flex justify-center">
+                <div class="w-full max-w-md pt-8">
+                    <h1 class="text-center text-2xl mb-4">{vnode.attrs.title}</h1>
+                    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        {vnode.children}
+                    </form>
+                </div>
+            </div>
+        </Page>
+    }
+
+}
--- frontend/src/pages/loginpage.tsx
+++ frontend/src/pages/loginpage.tsx
@@ -4,6 +4,7 @@ import { Page } from './page';
 import { AuthClient } from '../authclient';
 import { LabelledInput } from '../components/labelledinput';
 import { Button } from '../components/button';
+import { LoginPanel } from '../components/loginpanel';
 
 export class LoginPage {
 
@@ -15,31 +16,24 @@ export class LoginPage {
     }
 
     view() {
-        let complete = this.complete();
-        return <Page hideNavbar={true}>
-            <div class="flex justify-center">
-                <div class="w-full max-w-md pt-8">
-                    <h1 class="text-center text-2xl mb-4">Log in to staklist</h1>
-                    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" {...bind(this)}>
-                        <div class="mb-4">
-                            <LabelledInput label="Email" id="email" type="email" placeholder="me@awesome.com"/>
-                        </div>
-                        <div class="mb-4">
-                            <LabelledInput label="Password" id="password" type="password" placeholder="********"/>    
-                            {AuthClient.user.loginError && <p class="text-red-500 text-xs italic">{AuthClient.user.loginError}</p>}
-                            <a class="text-sm text-blue-500" href="/#!/forgotpassword">Forgot your password?</a>
-                        </div>
-                        <div class="flex items-center justify-between mb-4">
-                            <Button label="Log in" disabled={!this.complete()} callback={() => this.signup()}/>
-                        </div>
-                        <div class="text-center text-sm">
-                            Need an account? <a href="/#!/signup" class="text-blue-500">Sign up</a>
-                        </div>
-                    </form>
-
+        return <LoginPanel title="Log in to staklist">
+            <div {...bind(this)}>
+                <div class="mb-4">
+                    <LabelledInput label="Email" id="email" type="email" placeholder="me@awesome.com"/>
+                </div>
+                <div class="mb-4">
+                    <LabelledInput label="Password" id="password" type="password" placeholder="********"/>    
+                    {AuthClient.user.loginError && <p class="text-red-500 text-xs italic">{AuthClient.user.loginError}</p>}
+                    <a class="text-sm text-blue-500" href="/#!/forgotpassword">Forgot your password?</a>
+                </div>
+                <div class="mb-4">
+                    <Button label="Log in" disabled={!this.complete()} callback={() => this.signup()}/>
+                </div>
+                <div class="text-center text-sm">
+                    Need an account? <a href="/#!/signup" class="text-blue-500">Sign up</a>
                 </div>
             </div>
-        </Page>
+        </LoginPanel>
     }
 
     async signup() {
