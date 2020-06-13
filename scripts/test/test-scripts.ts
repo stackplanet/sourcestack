ba245dc63c10ab350e6583e2c7b6499d753df949
Martin Pllu <jmpllu@gmail.com>
2020-06-13 21:42:38 +0100

    Extract function

--- scripts/test/test-scripts.ts
+++ scripts/test/test-scripts.ts
@@ -1,14 +1,16 @@
 import { execute } from "../execute";
 import { writeFileSync } from "fs";
-import { Config } from "../config";
+
+
+function writeConfig(config: any){
+    writeFileSync(`test-override.json`, JSON.stringify(config));
+}
 
 (async () => {
-    let config = {
+    writeConfig({
         'name' : 'staktest',
         'environments' : 'alpha1',
-    };
-    
-    writeFileSync(`test-override.json`, JSON.stringify(config));
+    });
     await execute(`npm run destroy-env --env=alpha1 --configFileName=test-override.json --noConfirmDestroy`);
     await execute(`rm -rf api/dist.zip api/dist ui/dist ui/server.cert ui/server.key`)
     await execute(`npm run deploy --env=alpha1 --configFileName=test-override.json`);
