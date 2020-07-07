132dc783b56bb3475a331c7e877208c1719d6c29
Martin Pllu <jmpllu@gmail.com>
2020-07-07 11:47:23 +0100

    Fix copyright

--- ui/src/components/footer.tsx
+++ ui/src/components/footer.tsx
@@ -5,7 +5,7 @@ export default class Footer {
     view() {
         return <div>
             <div class="container h-20 px-6 py-2 flex justify-between items-center mx-auto">
-            Copyright Martin Pllu, 2020 <a class="underline" href="#">Legal stuff</a>
+            © Copyright stak megacorp, 2020 <a class="underline" href="#">Legal stuff</a>
             </div>
         </div>
     }
